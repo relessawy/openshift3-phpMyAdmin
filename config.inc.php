ccb08306d46e9ec63e2f11e45e1aaec3f869e464
wael2000 <waelibrahim2000@hotmail.com>
2018-05-19 12:38:08 +0400

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -26,11 +26,19 @@ $i = 0;
  */
 $i++;
 /* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* $cfg['Servers'][$i]['auth_type'] = 'cookie'; */
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'system-mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+
+
+$cfg['Servers'][$i]['port']          = '3306';
+$cfg['Servers'][$i]['connect_type']  = 'tcp';
+$cfg['Servers'][$i]['auth_type']     = 'config';
+$cfg['Servers'][$i]['user']          = 'root';
+$cfg['Servers'][$i]['password']      = 'e03hpwmt';
+
 
 /**
  * phpMyAdmin configuration storage settings.
